762ae7cf60e98ad81b838b73317213d3fbd73447
yusufby <yusufbayram19891989@gmail.com>
2023-07-17 12:54:16 +0100

    ant design

---
+++ src/App.js
@@ -0,0 +1,30 @@
+
+import './App.css';
+import { Input, Select } from 'antd';
+import { UserOutlined } from '@ant-design/icons';
+
+
+
+function App() {
+  const fruits = ['Banana', 'Mango', 'Orange', 'Cherry'];
+
+  return (
+    <div className="App">
+      <header>
+        <p>What is your favourite fruit?</p>
+        <Select 
+        mode='multiple' 
+        allowClear
+        maxTagCount={2}
+        placeholder='Select fruit' 
+        style={{width:"50%"}}>
+          {fruits.map((fruit, index) => {
+            return <Select.Option key={index} value={fruit}></Select.Option>
+          })}
+        </Select>
+      </header>
+    </div>
+  );
+}
+
+export default App;
