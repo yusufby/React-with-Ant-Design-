bb678229eb619511fa1935001c10f7828a4d85f5
yusufby <yusufbayram19891989@gmail.com>
2023-08-06 23:44:55 +0100

    dfd

--- src/App.js
+++ src/App.js
@@ -18,6 +18,7 @@ function App() {
         maxTagCount={2}
         placeholder='Select fruit' 
         style={{width:"50%"}}>
+        
           {fruits.map((fruit, index) => {
             return <Select.Option key={index} value={fruit}></Select.Option>
           })}
